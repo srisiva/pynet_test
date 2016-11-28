513e17061ca8bf24b3dab7849c8c7c2dd7f566d3
srisiva <sri777@gmail.com>
2016-11-28 09:21:34 -0800

    str examples

---
+++ str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+a='Cisco'
+b='Nexus7000'
+c='FEBi949483'
+d=raw_input("enter a fourth variable :")
+print '{0:<20}'.format(a)+'{0:20}'.format(b)+'{0:>20}'.format(c)+'{0:>20}'.format(d)
+
